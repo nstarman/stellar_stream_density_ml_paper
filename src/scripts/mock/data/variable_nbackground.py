532dbc5fd3e9e4cdd384dce01c49957b2447005f
nstarman <nstarman@users.noreply.github.com>
2023-08-14 13:47:15 -0400

    nbackground variable
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/scripts/mock/data/variable_nbackground.py
+++ src/scripts/mock/data/variable_nbackground.py
@@ -1,4 +1,4 @@
-"""write variable 'nstream_variable.txt' to disk."""
+"""write variable 'nbackground_variable.txt' to disk."""
 
 import asdf
 from showyourwork.paths import user as Paths  # noqa: N812
@@ -10,5 +10,5 @@ paths = Paths()
 
 with asdf.open(
     paths.data / "mock" / "data.asdf", lazy_load=False, copy_arrays=True
-) as af, (paths.data / "mock" / "nstream_variable.txt").open("w") as f:
-    f.write(f"{af['n_stream']}")
+) as af, (paths.data / "mock" / "nbackground_variable.txt").open("w") as f:
+    f.write(f"{af['n_background']}")
