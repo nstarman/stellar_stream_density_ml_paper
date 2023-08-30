c6c762b193e0ff5bcca2b0925ea1b405461fb4a7
nstarman <nstarman@users.noreply.github.com>
2023-08-30 15:32:28 -0400

    add commas
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/scripts/gd1/data/2.5-variable_ndata.py
+++ src/scripts/gd1/data/2.5-variable_ndata.py
@@ -13,4 +13,4 @@ from scripts import paths
 table = QTable.read(paths.data / "gd1" / "gaia_ps1_xm.asdf")
 
 with (paths.output / "gd1" / "ndata_variable.txt").open("w") as f:
-    f.write(f"{len(table)}")
+    f.write(f"{len(table):,}")
