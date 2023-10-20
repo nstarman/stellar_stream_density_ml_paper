34977aee9e2a32fd61d3cac7db76b31a56f79fbf
nstarman <nstarman@users.noreply.github.com>
2023-10-20 01:13:24 -0400

    cleanup pal5 plot
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/scripts/pal5/plot/photometric_background_selection.py
+++ src/scripts/pal5/plot/photometric_background_selection.py
@@ -39,8 +39,18 @@ fig, axs = plt.subplots(2, 1, figsize=(4, 4))
 # -------------------------------------------------
 # Phi1-phi2
 
-axs[0].set(xlabel=(r"$\phi_1 \ $ [deg]"), ylabel=(r"$\phi_2 \ $ [deg]"))
-axs[0].hist2d(data["phi1"][off_stream], data["phi2"][off_stream], cmap="gray", bins=300)
+axs[0].set(
+    xlabel=(r"$\phi_1 \ $ [deg]"),
+    ylabel=(r"$\phi_2 \ $ [deg]"),
+    rasterization_zorder=0,
+)
+axs[0].hist2d(
+    data["phi1"][off_stream],
+    data["phi2"][off_stream],
+    cmap="gray",
+    bins=300,
+    zorder=-10,
+)
 axs[0].plot(
     data["phi1"][~off_stream],
     data["phi2"][~off_stream],
@@ -49,7 +59,7 @@ axs[0].plot(
     ms=1e-2,
     color="tab:blue",
     alpha=0.5,
-    rasterized=True,
+    zorder=-5,
 )
 axs[0].grid(visible=True, which="both", axis="y")
 axs[0].grid(visible=True, which="major", axis="x")
@@ -58,15 +68,19 @@ axs[0].grid(visible=True, which="major", axis="x")
 # Photometric
 
 axs[1].set(
-    xlabel=(r"$g-r \ $ [mag]"), ylabel=(r"$g \ $ [mag]"), aspect="auto", xlim=(0, 0.8)
+    xlabel=(r"$g-r \ $ [mag]"),
+    ylabel=(r"$g \ $ [mag]"),
+    aspect="auto",
+    xlim=(0, 0.8),
+    rasterization_zorder=0,
 )
 axs[1].scatter(
     data["g"][off_stream] - data["r"][off_stream],
     data["g"][off_stream],
     s=1,
     color="k",
-    rasterized=True,
     label="off",
+    zorder=-10,
 )
 alpha = stream_kde(positions.T) - background_kde(positions.T)
 alpha[alpha < 0] = 0
@@ -76,8 +90,8 @@ axs[1].scatter(
     data["g"][~off_stream],
     s=1,
     alpha=0.05 + 0.95 * alpha,
-    rasterized=True,
     label="on-off",
+    zorder=-5,
 )
 axs[1].invert_yaxis()
 axs[1].yaxis.set_major_formatter(FormatStrFormatter("%d"))
